dbcd04c2aecda158e2ec4d2718f22fa2b81a0b02
chriseth <chris@ethereum.org>
2020-06-16 16:49:31 +0200

    Modules are pure.

--- libsolidity/analysis/TypeChecker.cpp
+++ libsolidity/analysis/TypeChecker.cpp
@@ -2713,6 +2713,8 @@ bool TypeChecker::visit(MemberAccess const& _memberAccess)
 				annotation.isPure = _memberAccess.expression().annotation().isPure;
 		}
 	}
+	else if (exprType->category() == Type::Category::Module)
+		annotation.isPure = _memberAccess.expression().annotation().isPure;
 
 	// TODO some members might be pure, but for example `address(0x123).balance` is not pure
 	// although every subexpression is, so leaving this limited for now.
@@ -3062,7 +3064,8 @@ bool TypeChecker::visit(Identifier const& _identifier)
 	}
 	else if (dynamic_cast<TypeType const*>(annotation.type))
 		annotation.isPure = true;
-
+	else if (dynamic_cast<ModuleType const*>(annotation.type))
+		annotation.isPure = true;
 
 	// Check for deprecated function names.
 	// The check is done here for the case without an actual function call.
--- libsolidity/codegen/ExpressionCompiler.cpp
+++ libsolidity/codegen/ExpressionCompiler.cpp
@@ -1718,6 +1718,19 @@ bool ExpressionCompiler::visit(MemberAccess const& _memberAccess)
 			solAssert(false, "Illegal fixed bytes member.");
 		break;
 	}
+	case Type::Category::Module:
+	{
+		Type::Category category = _memberAccess.annotation().type->category();
+		solAssert(
+			category == Type::Category::TypeType ||
+			category == Type::Category::Enum ||
+			category == Type::Category::Struct ||
+			category == Type::Category::Contract ||
+			category == Type::Category::Module,
+			""
+		);
+		break;
+	}
 	default:
 		solAssert(false, "Member access to unknown type.");
 	}
@@ -1930,6 +1943,10 @@ void ExpressionCompiler::endVisit(Identifier const& _identifier)
 		// no-op
 	}
 	else if (dynamic_cast<StructDefinition const*>(declaration))
+	{
+		// no-op
+	}
+	else if (dynamic_cast<ImportDirective const*>(declaration))
 	{
 		// no-op
 	}
