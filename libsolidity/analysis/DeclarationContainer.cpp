183ad8ca888fc7a609e043efba32ecee9baa136a
clubether <66947758+clubether@users.noreply.github.com>
2020-06-18 19:05:44 +0530

    Update DeclarationContainer.cpp

--- libsolidity/analysis/DeclarationContainer.cpp
+++ libsolidity/analysis/DeclarationContainer.cpp
@@ -15,8 +15,8 @@
 	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
 */
 /**
- * @author Christian <c@ethdev.com>
- * @date 2014
+ * @author ClubEther
+ * @date 2020
  * Scope - object that holds declaration of names.
  */
 
